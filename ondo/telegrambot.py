42f732517d0c73e5b7f9849bfad017867f993bb3
youngsoosoo <lovepys01@naver.com>
2023-11-26 23:32:42 +0900

    온도 이상 시 텔레그램 메시지 전송 기능 추가

--- ondo/telegrambot.py
+++ ondo/telegrambot.py
@@ -2,12 +2,37 @@ import telegram, sys, asyncio
 sys.path.append('/home/admin/201844079/raspberry-pi/util')
 from datetime import datetime
 import secret
+import ondo
+
 
 async def sendTelegramMessage():
+	
+	temp, humi = ondo.ondo()
+	print(temp, humi)
+	content = ''
+	formatted_now = datetime.now().strftime("%Y년 %m월 %d일 %H시 %M분 %S초")
+	
+	if temp > 25:
+		content = '\n현재 온도가 대파를 키우기엔 높습니다. \n적정 온도(5도~25도)를 맞춰주세요.'
+	elif temp < 5:
+		content = '\n현재 온도가 대파를 키우기엔 낮습니다. \n적정 온도(5도~25도)를 맞춰주세요.'
+	else:
+		content = '\n대파가 무럭무럭 성장하고 있습니다.'
+		
+	text = '안녕하세요\n'
+	text += '현재 시간: ' + formatted_now
+	text += '\n현재 온도: ' + str(temp)
+	text += '\n현재 습도: ' + str(humi) + '\n'
+	text += content
+	
 	token = secret.getToken()
 	chat_id = secret.getId()
 	bot = telegram.Bot(token=token)
-	text = '안녕하세요' + str(datetime.now())
+	
 	await bot.sendMessage(chat_id, text)
 
-asyncio.run(sendTelegramMessage())
+try:
+	asyncio.run(sendTelegramMessage())
+except telegram.error:
+	print("텔레그램 오류 발생. 재실행 시도...")
+	asyncio.run(sendTelegramMessage())
