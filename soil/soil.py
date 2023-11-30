eeb16171f8d04030ebe8054642eb5292e2723e6d
김건우 <201944085p@gmail.com>
2023-12-01 00:58:28 +0900

    데이터베이스 함수 추가

---
+++ soil/soil.py
@@ -0,0 +1,18 @@
+import serial, sys
+
+sys.path.append('../database')
+from function_savedata import savedata
+
+port = '/dev/ttyACM0'
+brate = 9600
+	
+def soil():
+	seri = serial.Serial(port, baudrate = brate, timeout = None)
+
+	while True:
+		if seri.in_waiting != 0:
+			line = seri.readline().decode()
+			soil_humi = map(float, line.split())
+			
+			savedata("soil", "soil_humi", soil_humi)
+			
